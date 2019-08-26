e548bf0f0d0c243c54262881b390d6cc70f81c44
Mark <shuyumark@gmail.com>
2019-08-26 14:12:25 +0800

    update

---
+++ src/main/java/io/github/ztmark/client/WriteDataInDiffWayClient.java
@@ -0,0 +1,13 @@
+package io.github.ztmark.client;
+
+/**
+ * Author: Mark
+ * Date  : 2019/8/26
+ */
+public class WriteDataInDiffWayClient {
+
+    public static void main(String[] args) {
+
+
+    }
+}
